dc0628b430a234cab5ac1f2a7e4d7e8d1be67620
Federico Tonti <federico.tonti@gmail.com>
2024-11-08 10:23:32 +0100

    Js esercizi 1-4

--- assets/js/D5.js
+++ assets/js/D5.js
@@ -10,19 +10,36 @@ REGOLE
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
-const pets = ['dog', 'cat', 'hamster', 'redfish']
+console.log("Esercizio 1");
+const pets = ['dog', 'cat', 'hamster', 'redfish'];
+  for(let i = 0; i < pets.length; i++)
+    console.log(pets[i]);
+
+
+
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
+console.log("Esercizio 2");
+const petsAlfa = pets.sort();
+console.log(petsAlfa);
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
+console.log("Esercizio 3");
+petsReverse = pets.reverse();
+console.log(petsReverse);
 
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+console.log("Esercizio 4");
+console.log(pets); //ARRAY PRIMA 
+const petsMoved = pets.shift();
+pets.push(petsMoved);
+console.log(pets); // ARRAY DOPO
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
